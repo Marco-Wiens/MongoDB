3fc775e0fbdfedbe9b6bde48d8ba3752efe3c7b1
Marco-Wiens <marco.wiens@gmail.com>
2023-11-30 18:10:59 +0100

    reto 3

--- esquema.js
+++ esquema.js
@@ -18,10 +18,23 @@ const ProfileSchema = new mongoose.Schema({
 
 const CredentialsSchema = new mongoose.Schema({
     address: String,
-    phone: Number,
+    phone: {
+        type: Number,
+        max: 999999999
+    },
     email: String
 
 })
+
+ProfileSchema.pre("save", (next) => {
+    console.log('Middleware de entrada');
+    if(this.rol == 'Admin' || this.rol == 'Empleado'){
+        console.log('Porfavor introduzca si es Admin o Empleado');
+    }else{
+        next();
+    }
+});
+
 
 let userSchema = mongoose.model("User", UserSchema);
 let profileSchema = mongoose.model("Profile", ProfileSchema);
